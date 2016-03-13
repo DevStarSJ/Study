6ed592309f39ac4128d43c944887ebaaaaf254b5
SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-03-13 17:40:12 +0900

    Q.04

---
+++ Blog/Algorithm/Top20CodingInterview/Q.04.Anagram/Q.04.Anagram.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    static bool IsAnagram(string str1, string str2)
+    {
+        if (str1.Length != str2.Length)
+            return false;
+        
+        string s1 = str1.ToLower();
+        string s2 = str2.ToLower();
+
+        Dictionary<char, int> dic = new Dictionary<char, int>();
+
+        foreach(char c in s1)
+        {
+            if (dic.ContainsKey(c))
+                dic[c] += 1;
+            else
+                dic.Add(c, 1);
+        }
+
+        foreach (char c in s2)
+        {
+            if (dic.ContainsKey(c))
+            {
+                dic[c] -= 1;
+                if (dic[c] < 0)
+                    return false;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(IsAnagram("Duty", "TYDU"));
+        System.Console.WriteLine(IsAnagram("LunaStar", "Demilune"));
+    }
+}
+
