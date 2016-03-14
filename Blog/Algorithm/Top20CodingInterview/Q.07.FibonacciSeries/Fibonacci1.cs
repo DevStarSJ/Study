a3ed3f23a8d5b62695b03f153c889bb3282d375d
seokjoon.yun <seokjoon.yun@gmail.com>
2016-03-14 09:46:12 +0900

    aa

--- Blog/Algorithm/Top20CodingInterview/Q.07.FibonacciSeries/Fibonacci1.cs
+++ Blog/Algorithm/Top20CodingInterview/Q.07.FibonacciSeries/Fibonacci1.cs
@@ -9,12 +9,31 @@
         return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 
+    public static int Fibonacci2(int n)
+    {
+        if (n < 3)
+            return n;
+
+        int nFirst = 1;
+        int nSecond = 2;
+
+        int nResult = 0;
+
+        for (int i = 3; i <= n; i++)
+        {
+            nResult = nFirst + nSecond;
+            nFirst = nSecond;
+            nSecond = nResult;
+        }
+        return nResult;
+    }
+
 static void Main(string[] args)
     {
         for (int i = 1; i <= 10; i++)
         {
-            System.Console.WriteLine(string.Format("Fibonacci {0} : {1}", i, Fibonacci(i)));
+            System.Console.WriteLine(string.Format("Fibonacci {0} : {1}", i, Fibonacci2(i)));
         }
-        System.Console.WriteLine(string.Format("Fibonacci 44th : {0}", Fibonacci(44)));
+        System.Console.WriteLine(string.Format("Fibonacci 44th : {0}", Fibonacci2(44)));
     }
 }
