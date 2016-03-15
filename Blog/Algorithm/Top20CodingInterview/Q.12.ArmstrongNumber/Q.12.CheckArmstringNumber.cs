675ac727ee59b9c6d2d6eed3ffde9342210fb010
seokjoon.yun <seokjoon.yun@gmail.com>
2016-03-15 09:55:13 +0900

    Q.12

---
+++ Blog/Algorithm/Top20CodingInterview/Q.12.ArmstrongNumber/Q.12.CheckArmstringNumber.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static bool IsArmstrongNumber(int n)
+    {
+        if (n < 100 || n > 999)
+            return false;
+
+        int n1 = n / 100;
+        int n2 = n % 100 / 10;
+        int n3 = n % 10;
+
+        int r1 = n1 * n1 * n1;
+        int r2 = n2 * n2 * n2;
+        int r3 = n3 * n3 * n3;
+
+        return n == r1 + r2 + r3;
+    }
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(string.Format("{0} is Armstrong Number ? {1}",
+            379, IsArmstrongNumber(379) ? "True" : "False"));
+
+        System.Console.WriteLine(string.Format("{0} is Armstrong Number ? {1}",
+            371, IsArmstrongNumber(371) ? "True" : "False"));
+    }
+}
+
