77a166e06a855e054fc67b165f1b54539bbd3bff
seokjoon.yun <seokjoon.yun@gmail.com>
2016-03-15 12:17:35 +0900

    Q.12

--- Blog/Algorithm/Top20CodingInterview/Q.12.ArmstrongNumber/Q.12.CheckArmstringNumber.cs
+++ Blog/Algorithm/Top20CodingInterview/Q.12.ArmstrongNumber/Q.12.CheckArmstringNumber.cs
@@ -1,19 +1,18 @@
 ﻿class Program
 {
-    static bool IsArmstrongNumber(int n)
+    static bool IsArmstrongNumber(int num)
     {
-        if (n < 100 || n > 999)
-            return false;
+        int step = num;
+        int sum = 0;
 
-        int n1 = n / 100;
-        int n2 = n % 100 / 10;
-        int n3 = n % 10;
+        while (step != 0)
+        {
+            int digit = step % 10;
+            sum += digit * digit * digit;
+            step /= 10;
+        }
 
-        int r1 = n1 * n1 * n1;
-        int r2 = n2 * n2 * n2;
-        int r3 = n3 * n3 * n3;
-
-        return n == r1 + r2 + r3;
+        return num == sum;
     }
     static void Main(string[] args)
     {
