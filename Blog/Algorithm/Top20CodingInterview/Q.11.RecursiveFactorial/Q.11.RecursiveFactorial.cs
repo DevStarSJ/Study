9b7e95b152e2a755fc06154513119a6e5fe09729
seokjoon.yun <seokjoon.yun@gmail.com>
2016-03-15 09:39:28 +0900

    Q.11

---
+++ Blog/Algorithm/Top20CodingInterview/Q.11.RecursiveFactorial/Q.11.RecursiveFactorial.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static int Factorial(int n)
+    {
+        return n == 1 ? 1 : n * Factorial(n - 1);
+    }
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(string.Format("{0} Factorial = {1}", 10, Factorial(10)));
+    }
+}
