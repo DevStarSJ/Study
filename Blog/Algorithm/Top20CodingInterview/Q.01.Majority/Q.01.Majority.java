515f25f4ca21d9364d431d081f6062428b3bf5d2
SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-03-17 07:50:14 +0900

    Update Q.01.Majority.java

--- Blog/Algorithm/Top20CodingInterview/Q.01.Majority/Q.01.Majority.java
+++ Blog/Algorithm/Top20CodingInterview/Q.01.Majority/Q.01.Majority.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 public class Q01_Majority {
 
-	public static int FindMajority(List<Integer> nList) throws Exception
+	public static int FindMajority(List<Integer> vNumbers) throws Exception
 	{
-		int nSize = nList.size();
+		int nSize = vNumbers.size();
 		int nMajority = nSize / 2;
-		HashMap<Integer, Integer> mFind = new HashMap<Integer, Integer>();
+		HashMap<Integer, Integer> mExistingNumbers = new HashMap<Integer, Integer>();
 		
 		for (int i = 0; i < nSize; i++)
 		{
-			int nElement = nList.get(i);
-			Integer nValue = mFind.get(nElement);
+			int nNumber = vNumbers.get(i);
+			Integer nValue = mExistingNumbers.get(nNumber);
 			if (nValue != null)
 			{
 				nValue++;
 				if (nValue >= nMajority)
-					return nElement;
-				mFind.put(nElement, nValue);
+					return nNumber;
+				mExistingNumbers.put(nNumber, nValue);
 			}
 			else
 			{
-				mFind.put(nElement, 1);
+				mExistingNumbers.put(nNumber, 1);
 			}
 		}
 
@@ -30,13 +30,13 @@ public class Q01_Majority {
 	
 	public static void main(String[] args) 
 	{
-		List<Integer> nList1 = Arrays.asList(1,2,3,4,4,4,4,4,3,3);
-		List<Integer> nList2 = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
+		List<Integer> nNumbers1 = Arrays.asList(1,2,3,4,4,4,4,4,3,3);
+		List<Integer> nNumbers2 = Arrays.asList(1,2,3,4,5,6,7,8,9,0);
 		
 		try 
 		{
-			System.out.println(FindMajority(nList1));
-			System.out.println(FindMajority(nList2));
+			System.out.println(FindMajority(nNumbers1));
+			System.out.println(FindMajority(nNumbers2));
 		}
 		catch (Exception e)
 		{
