4c459406aaa5b33493c8bc09817f02d4bf91334f
Seokjoon Yun <sj@zigbang.com>
2016-11-24 08:46:36 +0900

    AWS Lambda Python Handler

---
+++ Blog/Cloud/AWS/src/Lambda.Proxy.Python.py
@@ -0,0 +1,30 @@
+import json
+
+def get(event):
+    user_id = event['queryStringParameters']['id']
+    return { 'body': { 'id': user_id, 'name': "test" } }
+
+def post(event):
+    user_id = event['queryStringParameters']['id']
+    body = event['body']
+    header = event['headers']
+    return { 'body': { 'id': user_id, 'header': header, 'body': body } }
+
+route_map = {
+    '/test': {
+        'GET': get,
+        'POST': post
+    }
+};
+
+def router(event):
+    controller = route_map[event['path']][event['httpMethod']];
+    
+    if not controller:
+        return { 'body': { 'Error': "Invalid Path" } }
+    
+    return controller(event);
+
+def handler(event, context):
+    result = router(event);
+    return { 'body' : json.dumps(result) }
